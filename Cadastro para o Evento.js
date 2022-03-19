1b2c7b5de5bff747f545878d881023d125bdab4f
ferrmarcio <marcio.lf@hotmail.com>
2022-03-19 20:40:01 -0300

    Evento.js

--- Cadastro para o Evento.js
+++ Cadastro para o Evento.js
@@ -1,15 +1,29 @@
 //Cadastro para o Evento
-
-var dtAtual = '15/03/2022'
-var dtEvento ='20/03/2022' //data do envento
+//Datas atuais
+var diaAtual = 16
+var mesAtual = 03
+var anoAtual = 2022
+//Datas do evento
+var diaEvento = 19
+var mesEvento = 03
+var anoEvento = 2022
+//dados para cadastro dos convidados
 var idadePermitida = '18' //limite mínimo de idade permitida para o evento
 var idade = '19'          //idade do convidado
 var nConvidados = ['Marcio','Juliane','Lívia','Daniela','Douglas','Paulo']    //lista de convidados para o evento
 
-if (dtEvento >= dtAtual){
-    console.log(dtAtual ,'é uma data válida! O evento Poderá acontecer')
+if(diaEvento >= diaAtual){
+    if(mesEvento >= mesAtual){
+        if(anoEvento >= anoAtual){
+            console.log(diaAtual + '/' + mesAtual + '/' + anoAtual ,'é uma data válida! O evento Poderá acontecer')
+        }else{
+            console.log('Data inválida')
+        }
+    }else{
+        console.log('Data inválida')
+    }
 }else{
-    console.log('O evento não poderá acontecer, a data é inválida')
+    console.log('Data inválida')
 }
 
 if (idade >= idadePermitida){
